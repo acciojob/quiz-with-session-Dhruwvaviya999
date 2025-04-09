e66836ae9a6db202b1b974d03e63fbc34e9b5068
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-04-09 17:15:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -80,6 +80,6 @@ submitButton.addEventListener("click", () => {
     }
   });
 
-  scoreElement.textContent = Your score is ${score} out of ${questions.length}.;
+  scoreElement.textContent = `Your score is ${score} out of ${questions.length}`.;
   localStorage.setItem("score", score.toString());
 });
